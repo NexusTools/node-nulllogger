a5b303e45a28e82b32d3ff57c7394bb13688e95d
Katelyn Slater <ktaeyln@gmail.com>
2017-05-06 14:06:31 -0400

    Try using __dirname instead of ./

--- test/tests.js
+++ test/tests.js
@@ -4,41 +4,39 @@ var path = require('path');
 require("source-map-support").install();
 
 process.env.VERBOSE = "Gears"; // All possible verbosity levels
-process.env.LOGGER_NEVER_ASYNC = true;
 
 var pkg;
 var topDir = path.dirname(__dirname);
 var supportDir = path.resolve(__dirname, "support");
 var pkgfile = path.resolve(topDir, "package.json");
-it('parse package.json', function(){
+it('parse package.json', function () {
     pkg = require(pkgfile);
-    if(!pkg)
+    if (!pkg)
         throw new Error("Failed to parse `package.json`");
-    if(!("main" in pkg))
+    if (!("main" in pkg))
         throw new Error("`package.json` missing property `main`");
 });
 var logger;
-it("require main", function() {
+it("require main", function () {
     logger = require(topDir);
 });
-describe('api', function() {
-	
-    it("scope colors", function(){
-		for(var i=0;i<255;i++) {
-			var color = i;
-			logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
-			
-			color = "x" + i.toString(16);
-			logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
-		}
-		for(var color in logger.Color) {
-			if(isNaN(color)) {
-				logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
-			}
-		}
+describe('api', function () {
+    it("scope colors", function () {
+        for (var i = 0; i < 255; i++) {
+            var color = i;
+            logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
+
+            color = "x" + i.toString(16);
+            logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
+        }
+        for (var color in logger.Color) {
+            if (isNaN(color)) {
+                logger.log(logger.Level.Information, [color + ":" + color], ["Test"]);
+            }
+        }
     });
-	
-    it("static levels", function(){
+
+    it("static levels", function () {
         logger.gears("Test");
         logger.info("Test");
         logger.warn("Test", 44);
@@ -46,13 +44,13 @@ describe('api', function() {
         logger.debug(/.+/i);
         logger.perf(new String(373.5));
         logger.error("");
-		
+
     });
     var logInstance;
-    it("scope instance", function(){
+    it("scope instance", function () {
         logInstance = new logger("Mocha");
     });
-    it("scope log", function(){
+    it("scope log", function () {
         logInstance.gears("Test");
         logInstance.info("Test");
         logInstance.info("Test", []);
@@ -63,7 +61,7 @@ describe('api', function() {
         logInstance.perf(new String(373.5));
         logInstance.error("");
     });
-    it("extended scope", function(){
+    it("extended scope", function () {
         logInstance = logInstance.extend("green:Extended");
         logInstance.info("Test");
         logInstance.info("Test", []);
@@ -74,16 +72,16 @@ describe('api', function() {
         logInstance.perf(new String(373.5));
         logInstance.error("");
     });
-    it("timer", function(){
-		logInstance.timer("Test", function(logInstance) {
-			logInstance.info("Test");
-			logInstance.info("Test", []);
-			logInstance.info("Test", new Date());
-			logInstance.warn("Test", 44);
-			logInstance.error("Test", {farm: 43});
-			logInstance.debug(/.+/i);
-			logInstance.perf(new String(373.5));
-			logInstance.error("");
-		});
+    it("timer", function () {
+        logInstance.timer("Test", function (logInstance) {
+            logInstance.info("Test");
+            logInstance.info("Test", []);
+            logInstance.info("Test", new Date());
+            logInstance.warn("Test", 44);
+            logInstance.error("Test", {farm: 43});
+            logInstance.debug(/.+/i);
+            logInstance.perf(new String(373.5));
+            logInstance.error("");
+        });
     });
 });
